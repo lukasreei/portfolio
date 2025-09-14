5ed059de785c04843d2adc0e2cc02f715d6c25cb
Lukas <lukas581800@gmail.com>
2025-09-13 21:10:21 -0300

    fix:ajuste no texto header

--- portfolio/src/components/header.jsx
+++ portfolio/src/components/header.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/header.css";
+
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header>
       <h1>Lukas Santos</h1>
-      <nav>
+
+      {/* Botão hambúrguer */}
+      <div className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* Menu */}
+      <nav className={menuOpen ? "open" : ""}>
         <a href="#home">Home</a>
         <a href="#sobre">Sobre</a>
         <a href="#projects">Projetos</a>
-        <a href="#experiencias">Experiencias</a>
+        <a href="#experiencias">Experiências</a>
         <a href="#contact">Contato</a>
       </nav>
     </header>
